29d8decbacfa86719331443dfdd230f670240d14
dron1x <usaamidin@gmail.com>
2026-02-09 17:34:35 +0600

    class work.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("Book 1");
-        Book book2 = new Book("Book 2");
-        Book book3 = new Book("Book 3");
-        Book book4 = new Book("Book 4");
-        Book book5 = new Book("Book 5");
-        Book book6 = new Book("Book 6");
-        Book book7 = new Book("Book 7");
-
-        System.out.println(Book.getBookCount());
+        System.out.println(Calculator.add(3, 3));
+        System.out.println(Calculator.subtract(7,1));
+        System.out.println(Calculator.multiply(3,2));
+        System.out.println(Calculator.divide(12, 2));
     }
 }
