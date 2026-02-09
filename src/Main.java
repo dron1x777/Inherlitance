7cfa89251b7db3aac9f5904e1cc7ce41d7e207f4
dron1x <usaamidin@gmail.com>
2026-02-09 17:39:47 +0600

    class work.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Calculator.add(3, 3));
-        System.out.println(Calculator.subtract(7,1));
-        System.out.println(Calculator.multiply(3,2));
-        System.out.println(Calculator.divide(12, 2));
+        Parent.setMaxUsers(4);
     }
 }
---
+++ src/Parent.java
@@ -0,0 +1,8 @@
+public class Parent {
+    public static final double PI = 3.14;
+    public static final int MAX_USERS = 100;
+
+    public static final int setMaxUsers(int a){
+        this.MAX_USERS= a;
+    }
+}
